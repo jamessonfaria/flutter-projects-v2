de7ee5f2a4d90307da3c1fd0b0d6582b7c4fb089
Jamesson Faria <jamessonjr@gmail.com>
2023-05-23 17:59:29 -0300

    add first app

---
+++ first_app/lib/gradient_container.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'styled_text.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key, required this.colors});
+
+  final List<ColorSwatch> colors;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: colors,
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: StyledText('Hello World!'),
+      ),
+    );
+  }
+}
