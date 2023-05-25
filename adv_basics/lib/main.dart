fda353140fe7f838ed35a8a1d90d87d82d1e60db
Jamesson Faria <jamessonjr@gmail.com>
2023-05-25 12:16:03 -0300

    Adjust to refactor

--- adv_basics/lib/main.dart
+++ adv_basics/lib/main.dart
@@ -1,20 +1,6 @@
-import 'package:adv_basics/start_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:adv_basics/quiz.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color.fromARGB(255, 78, 13, 151),
-              Color.fromARGB(255, 107, 15, 168),
-            ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
